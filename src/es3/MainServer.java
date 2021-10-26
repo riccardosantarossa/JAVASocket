4f578a14fdd5952d4698f44d0841e7071e8de792
riccardosantarossa <rikysanta03@gmail.com>
2021-10-26 10:08:58 +0200

    Fa tutto tranne shutdown

--- src/es3/MainServer.java
+++ src/es3/MainServer.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -15,10 +16,11 @@ import java.util.Locale;
 public class MainServer 
 {
 	private static ServerSocket ws = null; // welcoming socket
-
+	private static boolean chiusura = true;
 	
 	public static void main(String[] args) 
 	{
+		
 		try {
 			ws = new ServerSocket(7979);
 		} catch (IOException e1) {
@@ -27,15 +29,23 @@ public class MainServer
 			System.out.println("Errore nel welcoming socket");
 		}
 		
-		while(ThreadClient.chiudiServer)
+		while(chiusura)
 		{
+			Socket s = null;
 			try 
 			{
-				Socket s = ws.accept();
-				ThreadClient c= new ThreadClient(s,ws);
+				s = ws.accept();
+				ThreadClient c= new ThreadClient(s);
 				Thread t = new Thread(c);
 				t.start();
-			} catch (IOException e) 
+			} 
+			
+			catch(SocketException e)
+			{
+				chiusura=false;
+			}
+			
+			catch (IOException e) 
 			{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -43,15 +53,18 @@ public class MainServer
 				 
 		}
 		
-		/*try 
-		{
+	}
+	
+	public static void ChiudiServer()
+	{
+		try {
 			ws.close();
-		} catch (IOException e) 
-		{
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}*/
+		}
 	}
+	
 	
 	
 	
--- src/es3/ThreadClient.java
+++ src/es3/ThreadClient.java
@@ -15,120 +15,93 @@ import java.util.Locale;
 
 public class ThreadClient implements Runnable {
 
-	private static Socket s = null;			// connection socket
-	private static ServerSocket ws = null;
+	private Socket s = null;			// connection socket
 	public static boolean chiudiServer=true;
 	
-	public ThreadClient(Socket s, ServerSocket ws)
+	public ThreadClient(Socket s)
 	{
 		this.s = s;
-		this.ws = ws;
 	}
 	
 	@Override
 	public void run() {
 		
-		while(true)
+		BufferedReader in = null;
+		PrintWriter out = null;
+		Date d = new Date();
+		
+		try 
 		{
-			BufferedReader in = null;
-			PrintWriter out = null;
-			Date d = new Date();
-			
-			
-			//Formato della data configurabile con DATEFORMAT
-			DateFormat formatoData = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, Locale.ITALY);
-			String data = formatoData.format(d);
-			
-			while(chiudiServer)
-			{
-			
-			try 
-			{
-				in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-				out = new PrintWriter(s.getOutputStream(), true);
-				out.println("Welcome to Riccardo Santarossa's server ");
-				System.out.println("remote host: "+s.getInetAddress());
-			} 
-			catch (IOException e) 
-			{			
-				e.printStackTrace();
-				System.out.println("Unable to get I/O stream on socket!");
-			}
-			
-			boolean chiudiConnessione=true;
-			
-			while(chiudiConnessione)
-			{
-				
-			try {
-				//out.println("Benvenuto in KeSuperSballo.Yeah.Ok!");
-				
-				// Per leggere da socket:
-				String str = in.readLine();
-				str.toLowerCase();
-				if(str.equals("data"))
-				{
-					//Scelgo se stampare data semplice o pi� complessa 
-					//out.println(new Date().toString());
-					out.println(data);
-				}
-				else if(str.equals("quit"))
-				{
-					//out.println(str); // ECHO
-					out.println("Goodbye");	
-					chiudiConnessione=false;
-				}
-				else if(str.equals("shutdown"))
-				{
-					chiudiConnessione=false;
-					chiudiServer=false;
-				}
-				else{
-					StringBuilder build = new StringBuilder(str);
-					build.reverse();
-					out.println(build);
-				}
-			} 
-			catch (IOException e) 
-			{
-				e.printStackTrace();
-				System.out.println("Communication Error!");
-			} 
-			}
-			
-			if(!chiudiServer && !chiudiConnessione)
-				try 
-				{
-					in.close();
-					out.close();  // close PrintWriter
-					s.close();    // close connection socket
-					//ws.close();
-				} 
-				catch (IOException e) 
-				{
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					System.out.println("Error closing socket!");
-				}
-			
-			if(!chiudiConnessione)
-				try 
-				{
-					out.close();  // close PrintWriter
-					s.close();    // close connection socket	
-				} 
-				catch (IOException e) 
-				{
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					System.out.println("Error closing socket!");
-				}
-			
-			}
+			in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			out = new PrintWriter(s.getOutputStream(), true);
+			System.out.println("remote host: "+s.getInetAddress());
+		} 
+		catch (IOException e) 
+		{			
+			e.printStackTrace();
+			System.out.println("Unable to get I/O stream on socket!");
 		}
-
-	}
-	
-	public static void main(String[] args) {}  // fine main
-
+		
+		out.println("Welcome to Riccardo Santarossa's server ");
+		
+		String str = null;
+		
+			while(true)
+			{	
+				
+				//Formato della data configurabile con DATEFORMAT
+				DateFormat formatoData = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT, Locale.ITALY);
+				String data = formatoData.format(d);
+				
+					try {
+						//out.println("Benvenuto in KeSuperSballo.Yeah.Ok!");
+						
+						// Per leggere da socket:
+						str = in.readLine();
+						str.toLowerCase();
+						if(str.equals("data"))
+						{
+							//Scelgo se stampare data semplice o complessa 
+							//out.println(new Date().toString());
+							out.println(data);
+						}
+						else if(str.equals("quit"))
+						{
+							//out.println(str); // ECHO
+							out.println("Goodbye");	
+							break;
+						}
+						else if(str.equals("shutdown"))
+						{
+							MainServer.ChiudiServer();
+						}
+						else
+						{
+							StringBuilder build = new StringBuilder(str);
+							build.reverse();
+							out.println(build);
+						}
+					}
+				
+				catch (IOException e) 
+				{
+					e.printStackTrace();
+					System.out.println("Communication Error!");
+				} 
+			}
+				
+					try 
+					{
+						in.close();
+						out.close();  // close PrintWriter
+						s.close();    // close connection socket
+					} 
+					catch (IOException e) 
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						System.out.println("Error closing socket!");
+					}	
+				
+		} 
 }
